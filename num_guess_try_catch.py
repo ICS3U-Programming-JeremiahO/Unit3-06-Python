df9d43191875211869b84e08647cde00e5967cf5
Jeremiah Omoike <jeremiah.omoike@stu.ocsb.ca>
2022-10-18 18:15:32 +0000

    number program try catch

--- num_guess_try_catch.py
+++ num_guess_try_catch.py
@@ -2,29 +2,36 @@
 # Created By: Jeremiah omoike
 # Date: October 14 2022
 # This program takes a guess number from user and checks if it is correct
+
 import random
 
 
 def main():
     # input user will pick a number between 0 and 9
     print("I am thinking of a number between 0 and 9.")
-    print("can you guess what it is?")
-    user_input = int(input("Please guess here:"))
+    print("Can you guess what it is?")
+    user_input_as_string = input("Please guess here:")
 
-    # process and display
-    random_variable = random.randint(0, 9)
+    # set a variable to a random number from 0 to 9
+    random_number = random.randint(0, 9)
+
     try:
-        user_input == random_variable
-        print("congratulations, you are correct!") 
+        # cast user integer from a string to an int
+        user_input_as_int = int(user_input_as_string)
+
+        if user_input_as_int == random_number:
+            print("Congratulations, you are correct!")
+        else:
+            print(
+                "Sorry, {} was not correct. The correct answer is {}".format(
+                    user_input_as_int, random_number
+                )
+            )
     except Exception:
         print("This is not an integer")
-    else:
-        print(
-            "sorry, {} was not correct. The correct answer is {}".format(
-                user_input, random_variable))
     finally:
         print("Thank you for playing my game")
-    
-            
+
+
 if __name__ == "__main__":
     main()
